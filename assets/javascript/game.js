1dd2be907ee0d0bde756c428909095e8b1fe87cd
Shelby Smith <shelbysmith@Shelbys-MacBook-Pro.local>
2018-02-09 09:09:31 -0600

    Clarified comments in js.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,16 @@
-//These are the variables for the game
+//These are the variables for the game.
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q","r", "s", "t", "u", "v", "w", "x", "y", "z" ] ;
 var wins = 0;
 var losses = 0;
 var guesses = 12;
 var guessedLetters = [];
 
-//This generates the computers guess
+//This generates the computers guess. This is done by multiplying a random number by the length of the options arrray, then rounding down to a whole number. That randomly genereated number corresponds with an index position in the options array, thus generating a random letter.
 var computerGuess = options[Math.floor(Math.random()*options.length)];
 //I left this in after testing, in case someone feels like cheating. Kinda like an easter egg!
 console.log("Letter I'm thinking of: " + computerGuess)
 
-//This allows the player to input their guesses
+//This allows the player to input their guesses. onkeyup detects that a key has been pressed. String.fromCarCode(event.keyCode). toLowerCase converts the key press to string data, to match the options array and ensures all data is recieved as a lowercase letter, to match the options array. This basically just gives us usable data to correspond with our options variable. This also adds the letter guessed to the gusssedLetters arrary, so the player can see which letters they have already guessed. 
 document.onkeyup = function() {
     var playerGuess = String.fromCharCode(event.keyCode).
         toLowerCase()
@@ -22,7 +22,7 @@ document.onkeyup = function() {
      alert("You win!");
      resetGame()
 
-//This happens if the player guessed incorrectly, and removes a guess
+//This happens if the player guessed incorrectly, and removes a guess. This happens because the above if statement came back false, triggering this to run.
  } else {
      guesses--;
  }
@@ -32,7 +32,7 @@ document.onkeyup = function() {
      alert("You lose!");
      resetGame()
  }
-//This function allows the game to start over
+//This function allows the game to start over. It resets the guesses variable to 12, empties the guessedLetters array, and creates a new computerGuess.
  function resetGame() {
     guesses = 12;
     guessedLetters = [];
