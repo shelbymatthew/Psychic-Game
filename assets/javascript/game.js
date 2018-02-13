d714aa183b16bbebf6630d1b8706c29d3e9e9948
Shelby Smith <shelbysmith@Shelbys-MacBook-Pro.local>
2018-02-13 10:39:56 -0600

    adding some game elements, basic html sturcture.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,37 +16,37 @@ document.onkeyup = function() {
         toLowerCase()
         guessedLetters.push(playerGuess);
 
-//This determines if the player has guessed correctly, adds a win, alerts the player that they have won, and calls the resetGame function  
- if (playerGuess === computerGuess) {
-     wins++;
-     alert("You win!");
-     resetGame()
+    //This determines if the player has guessed correctly, adds a win, alerts the player that they have won, and calls the resetGame function  
+    if (playerGuess === computerGuess) {
+        wins++;
+        alert("You win!");
+        resetGame()
 
-//This happens if the player guessed incorrectly, and removes a guess. This happens because the above if statement came back false, triggering this to run.
- } else {
-     guesses--;
- }
- //This deterimines if the player is out of guesses, adds a loss, alerts the player that they have lost, and calls the resetGame funciton
- if (guesses=== 0){
-     losses++;
-     alert("You lose!");
-     resetGame()
- }
-//This function allows the game to start over. It resets the guesses variable to 12, empties the guessedLetters array, and creates a new computerGuess.
- function resetGame() {
-    guesses = 12;
-    guessedLetters = [];
-    computerGuess = options[Math.floor(Math.random()*options.length)];
-    console.log("Letter I'm thinking of: " + computerGuess);
-  }
+    //This happens if the player guessed incorrectly, and removes a guess. This happens because the above if statement came back false, triggering this to run.
+    } else {
+        guesses--;
+    }
+    //This deterimines if the player is out of guesses, adds a loss, alerts the player that they have lost, and calls the resetGame funciton
+    if (guesses=== 0){
+        losses++;
+        alert("You lose!");
+        resetGame()
+    }
+    //This function allows the game to start over. It resets the guesses variable to 12, empties the guessedLetters array, and creates a new computerGuess.
+    function resetGame() {
+        guesses = 12;
+        guessedLetters = [];
+        computerGuess = options[Math.floor(Math.random()*options.length)];
+        console.log("Letter I'm thinking of: " + computerGuess);
+    }
 
-//This redoes the HTML of the page, to relect a playboard.
- var html = "<p>Try to guess what letting I'm thinking of!</p>" +
-        "<p>Wins: " + wins + "</p>" +
-        "<p>Loses: " + losses + "</p>" +
-        "<p>Guesses Remaining: " + guesses + "</p>" +
-        "<p>Guessed Letters: " + guessedLetters.join(', ') + "</p>" ;
-        
-        document.getElementById("game").innerHTML = html;
+    //This redoes the HTML of the page, to relect a playboard.
+    var html = "<p>Try to guess what letting I'm thinking of!</p>" +
+            "<p>Wins: " + wins + "</p>" +
+            "<p>Loses: " + losses + "</p>" +
+            "<p>Guesses Remaining: " + guesses + "</p>" +
+            "<p>Guessed Letters: " + guessedLetters.join(', ') + "</p>" ;
+            
+            document.getElementById("game").innerHTML = html;
            
 }
